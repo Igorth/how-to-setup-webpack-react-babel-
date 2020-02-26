dc6b3e3cb0d60780ee886218bcd1c6874517a7f0
Igorth <igordiasth@gmail.com>
2020-02-26 20:46:35 +1300

    import and export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import Form from "./js/components/Form";
+//import { soma, sub} from './js/components/funcoes';
+//import soma, { sub } from './js/components/soma'
+import * as funcoes from './js/components/funcoes'
+
+//console.log(soma(1, 2));
+//console.log(sub(4, 2));
+
+console.log(funcoes.mult(2,2));
