d8bd0d1e12397fd69228f1cf37575dd3d95f79d8
Igorth <igordiasth@gmail.com>
2020-02-27 17:52:40 +1300

    changelle about webpack server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 //import { soma, sub} from './js/components/funcoes';
 //import soma, { sub } from './js/components/soma'
-import * as funcoes from './js/components/funcoes'
+//import * as funcoes from './js/components/funcoes'
+//import soma from './js/components/soma'
 
 //console.log(soma(1, 2));
 //console.log(sub(4, 2));
 
-console.log(funcoes.mult(2,2));
+//console.log(funcoes.sub(2,2));
+
+//import { Usuario } from './js/components/functions';
+
+//console.log(Usuario.info());
+
+
+//import { soma } from './js/components/funcoes'
+//console.log(soma(1, 2))
+
+//import ClasseUsuario from './js/components/functions';
+//console.log(ClasseUsuario.info());
+
+//import { idade } from './js/components/functions';
+//console.log(idade);
+
+import Usuario, { idade as IdadeUsuario} from './js/components/functions'
+
+console.log(Usuario.info());
+console.log(IdadeUsuario);
