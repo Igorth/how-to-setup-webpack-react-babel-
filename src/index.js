49b47e4d31f1b74af5771e390b8e85248677039f
Igorth <igordiasth@gmail.com>
2020-02-27 18:33:51 +1300

    configuring axios

--- src/index.js
+++ src/index.js
@@ -45,10 +45,36 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 
 
-const executaPromise = async () => {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+// const executaPromise = async () => {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+// executaPromise();
+
+
+
+import axios from 'axios'
+
+class Api {
+    static async getUserInfo(username){
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+            console.log(response)
+        } catch (err) {
+            console.warn('Erro na API');
+        }        
+    }
 }
 
-executaPromise();
+Api.getUserInfo('Igorth');
+
+
+
+async function searchGit(username){
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    console.log(response);
+}
+
+searchGit('Igorth')
